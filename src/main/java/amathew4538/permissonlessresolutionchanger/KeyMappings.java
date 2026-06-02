00c16d0d4552128b09b3d4da7c5070b283b6e232
amathew4538 <amathew4538@colliervilledragons.org>
2026-06-01 19:16:15 -0500

    first commit

---
+++ src/main/java/amathew4538/permissonlessresolutionchanger/KeyMappings.java
@@ -0,0 +1,10 @@
+package amathew4538.permissionlessresolutionchanger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KeyMappings implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/amathew4538/permissonlessresolutionchanger/ResolutionChanger.java
@@ -0,0 +1,7 @@
+package amathew4538.permissionlessresolutionchanger;
+
+import java.net.Socket;
+
+public class ResolutionChanger {
+    
+}
