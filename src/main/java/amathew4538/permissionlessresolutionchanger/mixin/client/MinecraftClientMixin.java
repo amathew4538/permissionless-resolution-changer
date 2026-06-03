00b009d5d8863b74b7ff75d404a6460d83b23785
amathew4538 <amathew4538@colliervilledragons.org>
2026-06-02 23:10:46 -0500

    make keybinds do something

---
+++ src/main/java/amathew4538/permissionlessresolutionchanger/mixin/client/MinecraftClientMixin.java
@@ -0,0 +1,24 @@
+package amathew4538.permissionlessresolutionchanger.mixin.client;
+
+import amathew4538.permissionlessresolutionchanger.PermissionlessResolutionChanger;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void OnClientTick(CallbackInfo ci) {
+        if (PermissionlessResolutionChanger.baseSizeKeybind.wasPressed()) {
+            ResolutionChanger.setResolutionToBase();
+        } else if (PermissionlessResolutionChanger.tallSizeKeybind.wasPressed()) {
+            ResolutionChanger.setResolutionToTall();
+        } else if (PermissionlessResolutionChanger.thinSizeKeybind.wasPressed()) {
+            ResolutionChanger.setResolutionToThin();
+        } else if (PermissionlessResolutionChanger.wideSizeKeybind.wasPressed()) {
+            ResolutionChanger.setResolutionToWide();
+        }
+    }
+}
