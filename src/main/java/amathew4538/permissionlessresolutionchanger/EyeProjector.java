c43918e3eae53d074d4d2172babed3c0973f528d
amathew4538 <amathew4538@colliervilledragons.org>
2026-06-07 21:54:50 -0500

    fix errors

--- src/main/java/amathew4538/permissionlessresolutionchanger/EyeProjector.java
+++ src/main/java/amathew4538/permissionlessresolutionchanger/EyeProjector.java
@@ -3,17 +3,22 @@ package amathew4538.permissionlessresolutionchanger;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
+import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.glfw.GLFWErrorCallback;
 
 public class EyeProjector {
+    long clientWindowHandle;
+    long window;
+
     public static void StartProjector() {
-        long clientWindowHandle = Minecraft.getInstance().getWindow().getHandle();
+        clientWindowHandle = MinecraftClient.getInstance().getWindow().getHandle();
 
         Thread projectorThread = new Thread(() -> {
             GLFWErrorCallback.createPrint(System.err).set();
 		
             if(!GLFW.glfwInit()) {
-                    throw new IllegalStateException("Unable to initialize GLFW");
+                throw new IllegalStateException("Unable to initialize GLFW");
             }
 
             GLFW.glfwDefaultWindowHints();
@@ -22,7 +27,7 @@ public class EyeProjector {
             GLFW.glfwWindowHint(GLFW.GLFW_FOCUS_ON_SHOW, GLFW.GLFW_FALSE);
             GLFW.glfwWindowHint(GLFW.GLFW_FLOATING, GLFW.GLFW_TRUE);
 
-            window = GLFW.glfwCreateWindow(384, 384, "Eye Projector", NULL, clientWindowHandle);
+            window = GLFW.glfwCreateWindow(384, 384, "Eye Projector", MemoryUtil.NULL, clientWindowHandle);
             if(window == NULL) {
                 throw new IllegalStateException("Unable to create GLFW Window");
             }
@@ -30,8 +35,8 @@ public class EyeProjector {
             try {
                 GLFW.glfwSetWindowPos(
                     window,
-                    (ResolutionChanger.getScreenWidth() / 2 - 192) / 2,
-                    ResolutionChanger.getScreenHeight() / 2
+                    (Integer.parseInt(ResolutionChanger.getScreenWidth()) / 2 - 192) / 2,
+                    Integer.parseInt(ResolutionChanger.getScreenHeight()) / 2
                 );
 
                 GLFW.glfwMakeContextCurrent(window);
