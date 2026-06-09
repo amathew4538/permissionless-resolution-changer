99f8d0dd1d007a7ceb5bd952821fbd3f19620f95
amathew4538 <amathew4538@colliervilledragons.org>
2026-06-09 13:44:05 -0500

    we're so back!

--- src/main/java/amathew4538/permissionlessresolutionchanger/mixin/client/EyeProjectorMixin.java
+++ src/main/java/amathew4538/permissionlessresolutionchanger/mixin/client/EyeProjectorMixin.java
@@ -73,8 +73,8 @@ public class EyeProjectorMixin {
             int fbWidth = MinecraftClient.getInstance().getWindow().getFramebufferWidth();
             int fbHeight = MinecraftClient.getInstance().getWindow().getFramebufferHeight();
 
-            int readX = (fbWidth - 384) / 2;
-            int readY = (fbHeight - 384) / 2;
+            int readX = (fbWidth - 768) / 2;
+            int readY = (fbHeight - 768) / 2;
 
             GL15.glBindBuffer(GL21.GL_PIXEL_PACK_BUFFER, EyeProjector.pboId);
             GL11.glReadPixels(readX, readY, 768, 768, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, 0L);
