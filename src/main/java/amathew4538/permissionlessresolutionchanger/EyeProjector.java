8d38fb8eeb1c7df6cddb42ec4324d5fef61f0ec9
amathew4538 <amathew4538@colliervilledragons.org>
2026-06-08 22:51:49 -0500

    read screen?

--- src/main/java/amathew4538/permissionlessresolutionchanger/EyeProjector.java
+++ src/main/java/amathew4538/permissionlessresolutionchanger/EyeProjector.java
@@ -3,9 +3,14 @@ package amathew4538.permissionlessresolutionchanger;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL21;
 import net.minecraft.client.MinecraftClient;
 import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.system.MemoryStack;
+import java.nio.IntBuffer;
+import java.nio.ByteBuffer;
 
 public class EyeProjector {
     private static long clientWindowHandle;
@@ -34,7 +39,7 @@ public class EyeProjector {
         try {
             GLFW.glfwSetWindowPos(
                 window,
-                Integer.parseInt(ResolutionChanger.getScreenWidth()) / 2 - 576,
+                Integer.parseInt(ResolutionChanger.getScreenWidth()) / 2 - 768,
                 Integer.parseInt(ResolutionChanger.getScreenHeight()) / 2 - 192
             );
 
@@ -43,12 +48,29 @@ public class EyeProjector {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        int pboId = GL15.glGenBuffers();
+        GL15.glBindBuffer(GL21.GL_PIXEL_PACK_BUFFER, pboId);
+
+        int bufferSize = 384 * 384 * 4;
+        GL15.glBufferData(GL21.GL_PIXEL_PACK_BUFFER, bufferSize, GL15.GL_STREAM_READ);
+        GL15.glBindBuffer(GL21.GL_PIXEL_PACK_BUFFER, 0);
         
         Thread projectorThread = new Thread(() -> {
+            int projectorTextureId = 0;
             try {
                 GLFW.glfwMakeContextCurrent(window);
                 GL.createCapabilities();
-                
+
+                projectorTextureId = GL11.glGenTextures();
+                GL11.glBindTexture(GL11.GL_TEXTURE_2D, projectorTextureId);
+
+                GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+                GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+
+                GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, 384, 384, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, (ByteBuffer) null);
+                GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -56,11 +78,47 @@ public class EyeProjector {
             while (!GLFW.glfwWindowShouldClose(window)) {
                 GL11.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
                 GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-                
-                // Texture logic will go here
-                
+
+                int fbWidth = MinecraftClient.getInstance().getWindow().getFramebufferWidth();
+                int fbHeight = MinecraftClient.getInstance().getWindow().getFramebufferHeight();
+
+                if (fbWidth <= 0) fbWidth = 384;
+
+                GL15.glBindBuffer(GL21.GL_PIXEL_UNPACK_BUFFER, pboId);
+                GL11.glBindTexture(GL11.GL_TEXTURE_2D, projectorTextureId);
+                GL11.glTexSubImage2D(GL11.GL_TEXTURE_2D, 0, 0, 0, 384, 384, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, 0L);
+                GL15.glBindBuffer(GL21.GL_PIXEL_UNPACK_BUFFER, 0);
+
+                GL11.glViewport(0, 0, 384, 384);
+                GL11.glMatrixMode(GL11.GL_PROJECTION);
+                GL11.glLoadIdentity();
+                GL11.glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+                GL11.glMatrixMode(GL11.GL_MODELVIEW);
+                GL11.glLoadIdentity();
+
+                double scaleY = ((double) fbHeight / (double) fbWidth);
+                GL11.glScalef(1.0f, (float) scaleY, 1.0f);
+
+                GL11.glEnable(GL11.GL_TEXTURE_2D);
+                GL11.glBegin(GL11.GL_QUADS);
+                    GL11.glTexCoord2f(0.0f, 0.0f);
+                    GL11.glVertex2f(-1.0f, -1.0f);
+
+                    GL11.glTexCoord2f(1.0f, 0.0f);
+                    GL11.glVertex2f(1.0f, -1.0f);
+
+                    GL11.glTexCoord2f(1.0f, 1.0f);
+                    GL11.glVertex2f(1.0f, 1.0f);
+
+                    GL11.glTexCoord2f(0.0f, 1.0f);
+                    GL11.glVertex2f(-1.0f, 1.0f);
+
+                GL11.glEnd();
+                GL11.glDisable(GL11.GL_TEXTURE_2D);
+                GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+
                 GLFW.glfwSwapBuffers(window);
-                
                 try {
                     Thread.sleep(16);
                 } catch (InterruptedException e) {
@@ -69,6 +127,9 @@ public class EyeProjector {
                 }
             }
 
+            if (projectorTextureId != 0) {
+                GL11.glDeleteTextures(projectorTextureId);
+            }
             GLFW.glfwMakeContextCurrent(MemoryUtil.NULL);
         });
 
--- src/main/java/amathew4538/permissionlessresolutionchanger/mixin/client/EyeProjectorMixin.java
+++ src/main/java/amathew4538/permissionlessresolutionchanger/mixin/client/EyeProjectorMixin.java
@@ -19,6 +19,9 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL21;
 
 @Mixin(MinecraftClient.class)
 public class EyeProjectorMixin {
@@ -27,6 +30,16 @@ public class EyeProjectorMixin {
     @Inject(method = "tick", at = @At("HEAD"))
     private void OnClientTick(CallbackInfo ci) {
         GLFW.glfwPollEvents();
+
+        int fbWidth = MinecraftClient.getInstance().getWindow().getFramebufferWidth();
+        int fbHeight = MinecraftClient.getInstance().getWindow().getFramebufferHeight();
+
+        int readX = (fbWidth - 384) / 2;
+        int readY = (fbHeight - 384) / 2;
+
+        GL15.glBindBuffer(GL21.GL_PIXEL_PACK_BUFFER, pboId);
+        GL11.glReadPixels(readX, readY, 384, 384, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, 0L);
+        GL15.glBindBuffer(GL21.GL_PIXEL_PACK_BUFFER, 0);
 
         if (ResolutionChanger.isTallChanging && !isWaiting) {
             isWaiting = true;
