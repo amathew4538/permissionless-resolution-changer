7979b53a9f42e8e9120e87517eed9687b707aefc
amathew4538 <amathew4538@colliervilledragons.org>
2026-06-16 21:33:03 -0500

    middle 60 px now

--- src/main/java/amathew4538/permissionlessresolutionchanger/EyeProjector.java
+++ src/main/java/amathew4538/permissionlessresolutionchanger/EyeProjector.java
@@ -65,7 +65,7 @@ public class EyeProjector {
 
         GL15.glBufferData(GL21.GL_PIXEL_PACK_BUFFER, 2359296, GL15.GL_STREAM_READ);
         GL15.glBindBuffer(GL21.GL_PIXEL_PACK_BUFFER, 0);
-        
+
         Thread projectorThread = new Thread(() -> {
             int projectorTextureId = 0;
             try {
@@ -110,16 +110,16 @@ public class EyeProjector {
                 double scaleX = ((double) fbHeight / (double) fbWidth) * (windowWidth / 384);
 
                 GL11.glTranslatef(384.0f, 384.0f, 0.0f);
-                GL11.glScalef((float) scaleX, 1.0f, 1.0f);
+                GL11.glScalef((float) scaleX, 6.0f, 1.0f);
                 GL11.glTranslatef(-384.0f, -384.0f, 0.0f);
 
                 GL11.glEnable(GL11.GL_TEXTURE_2D);
                 GL11.glBindTexture(GL11.GL_TEXTURE_2D, projectorTextureId);
                 GL11.glBegin(GL11.GL_QUADS);
-                    GL11.glTexCoord2f(0.0f, 0.0f); GL11.glVertex2f(0.0f, 0.0f);
-                    GL11.glTexCoord2f(1.0f, 0.0f); GL11.glVertex2f(768.0f, 0.0f);
-                    GL11.glTexCoord2f(1.0f, 1.0f); GL11.glVertex2f(768.0f, 768.0f);
-                    GL11.glTexCoord2f(0.0f, 1.0f); GL11.glVertex2f(0.0f, 768.0f);
+                    GL11.glTexCoord2f(0.4609375f, 0.0f); GL11.glVertex2f(0.0f, 0.0f);
+                    GL11.glTexCoord2f(0.5390625f, 0.0f); GL11.glVertex2f(768.0f, 0.0f);
+                    GL11.glTexCoord2f(0.5390625f, 1.0f); GL11.glVertex2f(768.0f, 768.0f);
+                    GL11.glTexCoord2f(0.4609375f, 1.0f); GL11.glVertex2f(0.0f, 768.0f);
                 GL11.glEnd();
                 GL11.glDisable(GL11.GL_TEXTURE_2D);
                 GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
